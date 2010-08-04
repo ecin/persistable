b97321ea69cfc387d0b8615f326d3d56063e2607
ecin <ecin@copypastel.com>
2010-08-03 23:32:12 -0400

    Test::Unit should work find by now in Maglev. No more MiniTest requirement.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,4 @@
 $LOAD_PATH.unshift "../lib"
 
 require 'persistable'
-
-if !defined?(Maglev) or Maglev::VERSION.to_i > 22804
-  require 'test/unit'
-else
-  require 'minitest/unit'
-  MiniTest::Unit.autorun
-  Test = MiniTest
-end
+require 'test/unit'
